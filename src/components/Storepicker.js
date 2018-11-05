f20824fef80610d65f70ec443d9bdac27c062963
Leigh <lsteiner@locus.co>
2018-11-05 15:49:52 -0500

    first day

---
+++ src/components/Storepicker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class StorePicker extends React.Component {
+  render() {
+  	return (
+  	 <form className="store-selector">
+  	 	<h2>please enter a store</h2>
+  	 	<input type="text" required placeholder="Store Name" />
+  	 	<button type="submit">Visit Store</button>
+  	 </form>
+  	)
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom'
+import StorePicker from './components/Storepicker.js'
+
+import "./css/style.css"
+render(<StorePicker />, document.querySelector('#main')); 
